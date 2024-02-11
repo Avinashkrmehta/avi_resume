56c2831c1dac35e3efcc86d5f55778421063acb9
Avinashkrmehta <avi4791@gmail.com>
2024-02-12 00:07:12 +0530

    updated the resume

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_23_121356) do
+ActiveRecord::Schema.define(version: 2024_02_09_110130) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "feedbacks", force: :cascade do |t|
     t.string "name"
@@ -21,4 +24,36 @@ ActiveRecord::Schema.define(version: 2018_12_23_121356) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "shipments", force: :cascade do |t|
+    t.string "source_location"
+    t.string "target_location"
+    t.string "item"
+    t.integer "status"
+    t.bigint "user_id", null: false
+    t.bigint "delivery_partner_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["delivery_partner_id"], name: "index_shipments_on_delivery_partner_id"
+    t.index ["user_id"], name: "index_shipments_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at", precision: 6
+    t.datetime "remember_created_at", precision: 6
+    t.integer "role"
+    t.string "name"
+    t.string "phone_number"
+    t.text "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "onboarded"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "shipments", "users"
+  add_foreign_key "shipments", "users", column: "delivery_partner_id"
 end
